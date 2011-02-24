f8707f5ee58a8533bc884d4efea6aa648056a0ca
Fatih Kadir AKIN <fka@fatihkadirakin.com>
2011-02-24 23:03:20 +0200

    Front-end development.

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
-<?php
-
-echo 'bu server';
+<h1>What is Jack?</h1>
+<p>
+	Jack is a Javascript MVC development toolkit based on jQuery and Underscore.js. It allows you to write Javascript
+	applications using MVC structure.
+</p>
+<h1>What does Jack stands for?</h1>
+<p>
+	Jack is stands for <b>J</b>avascript <b>A</b>pplication <b>C</b>oding <b>K</b>it
+</p>
