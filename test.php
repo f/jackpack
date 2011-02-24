4e504ba53f3f3c74b9ea69be553497d61ebb5ccd
Fatih Kadir AKIN <fka@fatihkadirakin.com>
2011-02-25 01:39:18 +0200

    Front-end development.

--- test.php
+++ test.php
@@ -7,3 +7,7 @@
 <p>
 	Jack is stands for <b>J</b>avascript <b>A</b>pplication <b>C</b>oding <b>K</b>it
 </p>
+<h1>How it's running?</h1>
+<p style="text-align: center">
+	<img src="img/chalkboard.png" alt="sketch" style="width: 700px;" />
+</p>
