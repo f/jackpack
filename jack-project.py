7bbf3d5e41d78e632d59dab5706130b0b8717be6
Doğan Aydın <dogan1aydin@gmail.com>
2011-05-10 02:44:11 +0300

    Jack command-line tool

---
+++ jack-project.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os,sys,urllib2,argparse
+
+class JackProject:
+    def __init__(self,project_name):
+        self.project_name = project_name
+        
+    def init(self,obj,file_name):
+        self.obj = obj
+        self.file_name = file_name
+        if obj == "model":
+            self._download = urllib2.urlopen("https://github.com/fkadeveloper/jackpack/raw/master/templates/model.js").read()
+            self.download = self._download.replace("@PROJECT@",self.project_name).replace("@MODEL@",self.file_name)
+            self.download_to = "Model"
+        if obj == "controller":
+            self._download = urllib2.urlopen("https://github.com/fkadeveloper/jackpack/raw/master/templates/controller.js").read()
+            self.download = self._download.replace("@PROJECT@",self.project_name).replace("@CONTROLLER@",self.file_name)
+            self.download_to = "Controller"
+        if obj == "view":
+            self.download = urllib2.urlopen("https://github.com/fkadeveloper/jackpack/raw/master/templates/view.html").read()  
+            self.download_to = "View"
+        if obj == "jquery-plugin":
+            self._download = urllib2.urlopen("https://github.com/fkadeveloper/jackpack/raw/master/templates/jquery-plugin.js").read()
+            self.download = self._download.replace("@PLUGIN@",self.file_name)
+            self.download_to = "Plugins"
+        if obj == "underscore-plugin":
+            self.download = urllib2.urlopen("https://github.com/fkadeveloper/jackpack/raw/master/templates/underscore-plugin").read()
+            self.download = self._download.replace("@PLUGIN@",self.file_name)
+            self.download_to = "Plugins"
+            
+    def download_template(self):
+        if self.obj == "view":
+            write_to = "%s/%s.html" %(self.download_to,self.file_name)
+        else:
+            write_to = "%s/%s.js" %(self.download_to,self.file_name)
+        
+        open(write_to,"w").write(self.download)
+        
+
+parser = argparse.ArgumentParser(description='Create models,controllers,views,plugins...')
+parser.add_argument('--model',help='Create model')
+parser.add_argument('--controller',help='Create controller')
+parser.add_argument('--view',help='Create controller')
+parser.add_argument('--jquery',help='Create jquery plugin')
+parser.add_argument('--underscore',help='Create underscore plugin')
+arg = parser.parse_args()
+
+jackproject = JackProject(@PROJECTNAME@)
+
+if arg.model:
+    jackproject.init("model",arg.model)
+if arg.controller:
+    jackproject.init("controller",arg.controller)
+if arg.view:
+    jackproject.init("view",arg.view)
+if arg.jquery:
+    jackproject.init("jquery-plugin",arg.jquery)
+if arg.underscore:
+    jackproject.init("underscore-plugin",arg.underscore)
+    
+jackproject.download_template()
+
+
+
--- jack.py
+++ jack.py
@@ -1,112 +1,32 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import os,urllib2
 
-# TODO : Use(learn) command-line arg helper.
-
-import os,sys
-import jack_template as jt
-class JackGenerator (object):
-
-    def __init__(self,project_location="."):
-        if project_location != ".":
-            self.p_l = project_location
-        else:
-            self.p_l = str(os.getcwd().split("/")[len(os.getcwd().split("/"))-1:]).replace("['","").replace("']","")
-        self.project_location = project_location
-        self.controller_location = self.project_location + "/Controller"
-        self.model_location = self.project_location + "/Model"
-        self.view_location = self.project_location + "/View"
-        self.plugin_location = self.project_location + "/Plugins"
-        self.bootstrap_location = self.project_location
-            
-    def createBootstrap(self):  
-        self.bootstrap_template = jt.getTemplate("bootstrap_template",self.p_l) 
-        if os.system("touch %s/Bootstrap.js" % self.bootstrap_location) != 0:
-            print "Permission denied : %s/Bootstrap.js" % self.bootstrap_location
-        else:
-            bootstrap_file = open("%s/Bootstrap.js" % self.bootstrap_location,"a").write(self.bootstrap_template)
-            print "Bootstrap created."
+class JackPack:
+    def __init__(self,base_url,jack_url,project_name):
+        self.base_url = base_url
+        self.project_name = project_name
+        self.jack_url = jack_url
+    def download_jack(self):
+        jack_file = urllib2.urlopen(self.base_url).read()
+        j = open("Jack.tar","w").write(jack_file)
+        os.system("tar xf Jack.tar")
+         
+    def download_jack_project(self):
+        jack_project = urllib2.urlopen(self.jack_url).read().replace("@PROJECTNAME@",self.project_name)
+        download_to = "%s/jack-project.py" % self.project_name
+        j = open(download_to,"w").write(jack_project)
+        
+    def create_project(self):
+        if os.mkdir(self.project_name) == None:
+            os.mkdir("%s/Controller") % self.project_name
+            os.mkdir("%s/Model") % self.project_name
+            os.mkdir("%s/View") % self.project_name
+            os.mkdir("%s/Plugins") % self.project_name
+            self.download_jack()
+            self.download_jack_project()    
+        
     
-    def createProject(self):
-        try:
-            print "Creating project files..."
-            os.makedirs(self.controller_location)
-            os.makedirs(self.model_location)
-            os.makedirs(self.view_location)
-            os.makedirs(self.plugin_location)
-            self.createBootstrap()
-            print "Created."
-        except OSError:
-            print "Permission denied : %s" % self.project_location
-   
-    def createController(self,controller):
-        self.controller_template = jt.getTemplate("controller_template",self.p_l,controller)
-        if os.system("touch %s/%s.js" % (self.controller_location,controller)) != 0:
-            print "Permission denied : %s/%s.js" % (self.controller_location,controller)
-        else:
-            open("%s/%s.js" % (self.controller_location,controller),"a").write(self.controller_template)
-            print "%s controller created." % controller
-    
-    def createModel(self,model):
-        self.model_template = jt.getTemplate("model_template",self.p_l,model)
-        if os.system("touch %s/%s.js" % (self.model_location,model)) != 0:
-            print "Permission denied : %s/%s.js" % (self.model_location,model)
-        else:
-            open("%s/%s.js" % (self.model_location,model),"a").write(self.model_template)
-            print "%s model created." % model
-
-    def createView(self,view):
-        self.view_template = jt.getTemplate("view_template",self.p_l,view)
-        if os.system("touch %s/%s.js" % (self.view_location,view)) != 0:
-            print "Permission denied : %s/%s.js" % (self.view_location,view)
-        else:
-            open("%s/%s.js" % (self.view_location,view),"a").write(self.view_template)
-            print "%s view created." % view
-            
-    def createPlugin(self,plugin):
-        self.plugin_template = jt.getTemplate("plugin_template",self.p_l,plugin) 
-        if os.system("touch %s/%s.js" % (self.plugin_location,plugin)) != 0:
-            print "Permission denied : %s/%s.js" % (self.plugin_location,plugin)
-        else:
-            open("%s/%s.js" % (self.plugin_location,plugin),"a").write(self.plugin_template)
-            print "%s plugin created." % plugin            
-
-
-if len(sys.argv) > 2:
-    if sys.argv[1] == "Controller":
-        o = JackGenerator()
-        o.createController(sys.argv[2])
-    elif sys.argv[1] == "View":
-        o = JackGenerator()
-        o.createView(sys.argv[2])
-    elif sys.argv[1] == "Model":
-        o = JackGenerator()
-        o.createModel(sys.argv[2])
-    elif sys.argv[1] == "Plugin":
-        o = JackGenerator()
-        o.createPlugin(sys.argv[2])
-if len(sys.argv) == 2:
-    o = JackGenerator(sys.argv[1])
-    o.createProject()
-    o.createModel("%s" % sys.argv[1])
-    o.createView("%s" % sys.argv[1])
-    o.createController("%s" % sys.argv[1])
-    o.createPlugin("%s" % sys.argv[1])
-
-if len(sys.argv) == 1:
-    print "Usage: python jack.py ProjectName"
-    print "       python jack.py Controller ControllerName"
-    print "       python jack.py View ViewName"
-    print "       python jack.py Model ViewName"
-    print "       python jack.py Plugin PluginName"
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+a = JackPack("https://github.com/fkadeveloper/jackpack/raw/template/js/Jack.tar","Deneme_Project")
+a.create_project()
